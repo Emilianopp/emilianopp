16deed570d390beced568a264363d78b9ec3c52a
Emilianopp <emilianopp550@gmail.com>
2021-09-22 18:33:46 -0400

    cleanup

--- src/App.js
+++ src/App.js
@@ -4,25 +4,17 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import './custom.css'
 import Navigation from './Navigation';
 
-import Footer from './Footer'
-import BasicTimeline from './timeline';
-
-import Animated_slides from  './Animation/Animation_slides.jsx';
-
-import Animated_skills from  './Animation/Animation_skills';
 import { HashRouter as Router,Route,Switch,Link,Redirect } from 'react-router-dom';
 import Home_page from './Home_page';
 import Projects from './projects';
 import Resume from './Resume';
-import  Stats from './stats'
-import {Helmet} from "react-helmet";
+
 const App = () => {
 
   return (
     <>
     <div className = "app">
-    <Home_page/>
-    {/* <Router basename="/" >
+    <Router  basename='/'>
       <Switch>
      
      
@@ -33,7 +25,7 @@ const App = () => {
 
       <Redirect to = "/home" ></Redirect>
      </Switch>
-    </Router> */}
+    </Router>
     </div>
       </>
       
