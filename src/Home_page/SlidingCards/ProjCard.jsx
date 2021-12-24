1715e50e449bad3374a0451623db847bfa847a44
emilianopp <emilianopp550@gmail.com>
2021-12-24 15:58:54 -0500

    project card components

---
+++ src/Home_page/SlidingCards/ProjCard.jsx
@@ -0,0 +1,30 @@
+import  React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import 'styles/Home/ProjectCards.scss'
+
+export default function ProjCard({item}){
+    return (
+<Card className = "Project-card-content" >
+    
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Lizard
+          {item.name}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+    )
+}
