0e013e642645237e5b11e641960995061cedfaa3
emilianopp <emilianopp550@gmail.com>
2021-12-25 15:49:37 -0500

    formatting of main page

--- src/Home_page/index.js
+++ src/Home_page/index.js
@@ -1,33 +1,49 @@
 import Intro from "./Intro/Intro";
-import React from "react";
+
 import Navigation from "../Navigation";
 import "styles/Home/home_page.scss";
 import { Row, Col, Container } from "react-bootstrap";
 import SlidingCards from "./SlidingCards/SlidingCards";
-import ProjCard from "./SlidingCards/ProjCard";
+
+
+import PersonalTimeline from "./TimeLine/Timeline";
+import Skills from "./Skills/Skills";
 function Home_page() {
   return (
     <>
       <Container className="home_page" fluid>
+        {/* sidebar columns*/}
         <Row>
-          <Col md={2} xl={2} id="sidebar-wrapper">
+          <Col md={2} xl={2} xs={0} id="sidebar-wrapper">
             <Navigation />
           </Col>
-
-          <Col xl={10} md={10} id="page-content-wrapper">
+          {/* particles wrapper */}
+          <Col xl={10} md={10} xs={12} id="page-content-wrapper">
             <Row>
               <Intro />
             </Row>
+            {/* project cards */}
             <Row>
               <Container className="project-cards">
-                <Row className = "Cards">
-                  <Col  xl = {8} md = {8}>
-                  
-
+                <Row className="Cards">
+                  <Col xl={{ span: 8, offset: 2 }} md={{ span: 8, offset: 2 }}>
                     <SlidingCards />
                   </Col>
-                 </Row>
-                
+                </Row>
+              </Container>
+            </Row>
+
+            <Row>
+              <Container className="project-cards">
+                <Row className="Cards">
+                  <Col xl={{ span: 6 }} md={{ span: 6, offset: 0 }} xs={12}>
+                    <PersonalTimeline />
+                  </Col>
+
+                  <Col xl={{ span: 6 }} md={{ span: 6, offset: 0 }} xs={12}>
+                    <Skills />
+                  </Col>
+                </Row>
               </Container>
             </Row>
           </Col>
