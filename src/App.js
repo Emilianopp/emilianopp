41036de59d5ef48741da91d421e6c7cf025ab9cd
emilianopp <emilianopp550@gmail.com>
2021-12-28 18:34:17 -0500

    changes to pathing

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ import Navigation from './Navigation';
 
 import { HashRouter as Router,Route,Switch,Link,Redirect } from 'react-router-dom';
 import Home_page from './Home_page';
-import Projects from './projects';
-import Resume from './Resume';
+
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
 
 
@@ -21,9 +20,7 @@ const App = () => {
      
      
      <Route exact path = "/home" component = {Home_page}></Route>
-      <Route exact path = "/projects" component = {Projects}></Route>
-      <Route exact path = "/resume" component = {Resume}></Route>
-     
+      
 
       <Redirect to = "/home" ></Redirect>
      </Switch>
